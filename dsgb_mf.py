33af574596d0187a5af66ed84ff62eb7a0b05606
lpxbetter <marrowxiong@gmail.com>
2015-04-18 01:32:59 -0400

    update

--- dsgb_mf.py
+++ dsgb_mf.py
@@ -1,12 +1,9 @@
-#lipingxiong
-
-from pyspark import SparkContext
+from pyspark import SparkContext 
 import logging
 import numpy as np
-#from pyspark import *
 import sys
 import os
-#from common import *
+import datetime
 
 """
 spark-submit dsgd_mf.py <num_factors> <num_workers> <num_iterations> \
@@ -15,9 +12,10 @@ spark-submit dsgd_mf.py <num_factors> <num_workers> <num_iterations> \
 
 spark-submit dsgd_mf.py 100 10 50 0.8 1.0 test.csv w.csv h.csv
 """
+starttime = datetime.datetime.now()
 
 #The typical solution would be mapPartitions so for each worker the function you give mapPartitions is executed in parallel but inside each worker you define a method that sequentially performs the sgd updates within an active block
-sc = SparkContext(appName="dsgd_mf")
+sc = SparkContext(appName="PythonPi")
 num_factors=int(sys.argv[1])
 num_workers=int(sys.argv[2])
 num_iterations=int(sys.argv[3])
@@ -27,29 +25,24 @@ in_Vfile=sys.argv[6]
 out_Wfile=sys.argv[7]
 out_Hfile=sys.argv[8]
 tempdir="./"
+#file=sys.argv[8]
 
-#load the text file
 path = os.path.join(tempdir, in_Vfile)
 V = sc.textFile(path)
 #logging.debug( "count=%d"% V.count())
 
 #tmpRdd=V.map(lambda line : (line.split(",")[0],line.split(",")[1], line.split(",")[2]) ).cache()
 arrRdd=V.map(lambda line : map(int,line.split(','))).cache()
-#get the Ni and Nj
 Ni_li=arrRdd.map(lambda x : (x[0],1)).reduceByKey(lambda x,y:x+y).collect()
 Nj_li=arrRdd.map(lambda x : (x[1],1)).reduceByKey(lambda x,y:x+y).collect()
-#get the maxRow and maxCol
 maxRow=arrRdd.map(lambda x : x[0]).reduce(lambda x,y:max(x,y))
 maxCol=arrRdd.map(lambda x : x[1]).reduce(lambda x,y:max(x,y))
 
-#calculate the rowsize and colsize of the block
 block_rowsize=maxRow/num_workers
 block_colsize=maxCol/num_workers
-
-# Intitilize W and H, W: M*F, H: F*N
+#W: M*F, H: F*N
 matrixW= np.random.random((maxRow,num_factors)) 
-matrixH= np.random.random((num_factors,maxCol))
-#transform to dict for using later
+matrixH= np.random.random((num_factors,maxCol)) 
 W_dict={}
 H_dict={}
 for i in range(maxRow):
@@ -65,83 +58,101 @@ for e in Ni_li:
 for e in Nj_li:
 	Nj_dict[e[0]]=e[1]
 
-# broadcast Ni and Nj
 Nibd=sc.broadcast(Ni_dict)
 Njbd=sc.broadcast(Nj_dict)
-mPerStrata=0
-#for e in Ni_li:
 #	mPerStrata+=Ni_li[]
 
-# the sgd function for each worker
+liColInterval = []
+blockColSize = int( (1+maxRow) / num_workers)
+for i in xrange(num_workers):
+	if i == num_workers -1:
+		liColInterval.append((blockColSize*i,maxRow))
+	else:
+		liColInterval.append((blockColSize*i,blockColSize*(i+1)-1))
+liColIntervalbd=sc.broadcast(liColInterval)
+
 def sgd(idx,D):
+	liColInterval=liColIntervalbd.value
 	key=idx
 	#logging.debug( "key=%s"%key)
-    # get W,H,Ni,Nj from broadcast
 	W=Wbd.value
-	H=Wbd.value
+	H=Hbd.value
 	Ni=Nibd.value
 	Nj=Njbd.value
 	mCnt=mCntbd.value
-    #get permutation sequence from the broadcast
 	permArr=permbd.value
 	perm=permArr[key]
-	J=[]
-    # get the block rows and cols this worker should work on
-	for j in range(block_colsize):
-		J.append(block_colsize*perm+j+1)
-    # this two dicts are the W_I and H_J that will be updated
+	#J=[]
+	#for j in range(block_colsize):
+	#	J.append(block_colsize*perm+j+1)
 	WI_new={}
 	HJ_new={}
-	T0=100
+	T0=500
 	T=Tbd.value
 	epsilon = (100 + mCnt + T)**(-beta)
-	loss=0
+	curM=0
 	for d in D:
 		#print d
 		i=int(d[1][0])		
 		j=int(d[1][1])
 		rating=float(d[1][2])
-		if(j in J):
+		#if(j in J):
+		if(j>=liColInterval[perm][0] and j<= liColInterval[perm][1]):
 			dotV=np.dot(W[i],H[j])
 			eij = rating - dotV
 			logW=-2*eij * H[j] + 2*lamb/Ni[i] * W[i]
 			logH=-2*eij * W[i] + 2*lamb/Nj[j] * H[j]
 			WI_new[i]=W[i] - epsilon*logW
 			HJ_new[j]=H[j] - epsilon*logH
-			loss+=eij
-    #return the updated W_I and H_J and the loss
-	return [('w',WI_new),('h',HJ_new),('l',loss)]
+			curM+=1
+	return [('w',WI_new),('h',HJ_new),('m',curM)]
 
-# do the partition
 def partFunc(key):
+	block_rowsize = int( maxRow + 1) / num_workers
 	return key/block_rowsize
 partRdd=arrRdd.map(lambda x:(x[0],x)).partitionBy(num_workers,partFunc).cache()
-
 #begin iteration
-mBef=0
+m=0
 for t in range(num_iterations):
 	Wbd=sc.broadcast(W_dict)
 	Hbd=sc.broadcast(H_dict)
 	#permutation
-    permbd=sc.broadcast(permArr)
-    #broadcase W,H,permutation
-    newWH=partRdd.mapPartitionsWithIndex(sgd,True).collect()
-        
 	permArr=np.random.permutation(num_workers)
-	mCntbd=sc.broadcast( (mTot/num_workers) * (t+1) )
-	Tbd=sc.broadcast(mTot/(num_workers*num_workers)  )
-
+	#mCntbd=sc.broadcast( (mTot/num_workers) * (t+1) )
+	#Tbd=sc.broadcast(mTot/(num_workers*num_workers)  )
+	mCntbd=sc.broadcast(m)
+	Tbd=sc.broadcast(t)
+	permbd=sc.broadcast(permArr)
+	#broadcase W,H,permutation
+	newWH=partRdd.mapPartitionsWithIndex(sgd,True).collect()
 	for e in newWH:
 		#logging.debug("e[0=]%s"%e.type)
 		if(e[0]=='w'):
 			W_dict.update(e[1])
-		if(e[0]=='h'):
+		elif(e[0]=='h'):
 			H_dict.update(e[1])
+		elif(e[0]=='m'):
+			m+=e[1]
+endtime = datetime.datetime.now()
+#print "%s"%(endtime-starttime).seconds
 
+#loss
+V=np.zeros((maxRow,maxCol))
+#print "maxRow=%d,maxCol=%d"%(maxRow,maxCol)
+file = open(path)
+L=0
+for line in file:
+	i,j,rating=map(int,line.split(','))
+	V[i-1][j-1]=float(rating)
+	L+=(rating- np.inner(W_dict[i],H_dict[j] )  )**2
+#print "loss=%s"% L
+print L
+
+
+"""
 #output
 outw = open(out_Wfile, 'w+')
 outh = open(out_Hfile, 'w+')
-# write W to w.csv
 for i in range(maxRow):
 	i=i+1
 	if(W_dict.has_key(i)):
@@ -151,18 +162,39 @@ for i in range(maxRow):
 	else:
 		newList=['0']*num_factors
 		outw.write(','.join(newList)+'\n')
-
-# write H to h.csv
+	
 tmp=matrixH.T
 for j in range(maxCol):
 	if(H_dict.has_key(j+1)):
 		tmp[j]=H_dict[j+1]
-		print H_dict[j+1]
+		#print H_dict[j+1]
 	else:
 		tmp[j]=[0]*num_factors
 final=tmp.T
 for j in range(num_factors):
 	newList=[str(x) for x in final[j]]
 	s=','.join(newList)
-	print s
+	#print s
 	outh.write(s+'\n')
+"""
+
+'''
+def compL()
+V=np.zeros((maxRow,maxCol))
+#print "maxRow=%d,maxCol=%d"%(maxRow,maxCol)
+file = open(path)
+L=0
+for line in file:
+	i,j,rating=map(int,line.split(','))
+	V[i-1][j-1]=float(rating)
+	L+=(rating- np.inner(W_dict[i],H_dict[j] )  )**2
+print "loss=%s"% L
+W=np.genfromtxt(out_Wfile, dtype=np.float64, delimiter=",")
+H=np.genfromtxt(out_Hfile, dtype=np.float64, delimiter=",")
+
+Vhat=W.dot(H)
+nonzeroIdx=V.nonzero()
+loss=np.asarray(V[nonzeroIdx] - Vhat[nonzeroIdx])
+'''
+#print "loss=%s"% np.sum(loss**2)
+
